7ffa25bf36dfbef576c472c97e65439bc05ac8bf
Alexsander <alexsander11115@gmail.com>
2019-09-11 13:20:44 +0300

    Add work code

--- index.php
+++ index.php
@@ -2,48 +2,49 @@
 include 'vendor/autoload.php';
 require('vendor/electrolinux/phpquery/phpQuery/phpQuery.php');
 include 'function.php';
-
+//$page= file_get_contents("https://www.ferplast.com/ru/sobaki.html" );
+// $document = phpQuery::newDocument($page);
+//    $ean = $document->find('.product-ean');
+//    $ean = $ean->getString();
+//
+//    debug(empty($ean));
 
 $xml = file_get_contents('sitemap.xml');
 $service = new Sabre\Xml\Service();
-$result = $service->parse($xml);
+$results = $service->parse($xml);
 
 
-$page= file_get_contents('https://www.ferplast.com/ru/kontejnery-dlja-korma/feedy.html');
 
-$document = phpQuery::newDocument($page);
 
-$ean = $document->find('.product-ean');
-$sku = pq('h1')->getString();
 //preg_match_all("/(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/",$xml,$matches);
 
-foreach ($ean as $el) {
-    debug($el->textContent) . '<br>';
+echo "<table border='1'>    
+    <tr>
+        <th>Url</th>
+        <th>EAN</th>
+        <th>Описание</th>
+    </tr>";
+
+foreach ($results as $result){
+    $page= file_get_contents($result['value'][0]['value'] );
+    $document = phpQuery::newDocument($page);
+
+    $ean = $document->find('.product-ean');
+    $ean = $ean->getString();
+    $description = $document->find('#label_tab_1');
+    $description = $description->getString();
+    if(empty($ean)){
+        continue;
+    }
+    echo "
+    <tr>
+        <td>".$result['value'][0]['value'] ."</td>
+        <td>".$ean[0]."</td>
+        <td>".$description[0]."</td>
+    </tr>";
 }
-//foreach ($sku as $el) {
-//
-//        debug($el->childNodes[0]) . '<br>';
-//
-//
-//}
-
-debug($page);
-
+echo "</table>"
 ?>
-<!--<table border="1">-->
-<!--    <caption>Таблица https://www.ferplast.com</caption>-->
-<!--    <tr>-->
-<!--        <th>Url</th>-->
-<!--        <th>SKU</th>-->
-<!--        <th>EAN</th>-->
-<!--        <th>Описание</th>-->
-<!--        <th>Характеристики</th>-->
-<!--    </tr>-->
-<!--    <tr>-->
-<!--        <td>34,5</td>-->
-<!--        <td>3,5</td>-->
-<!--        <td>36</td>-->
-<!--        <td>23</td>-->
-<!--        <td>23</td>-->
-<!--    </tr>-->
-<!--</table>-->
+
+
+
